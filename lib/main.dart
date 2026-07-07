b01fed4b94c69d33a28d1564c8f4425eea3adeb0
柏原萌 <kashiwabaramoe@kashiwabaramoenoMacBook-Air.local>
2026-07-07 18:55:30 +0900

    emotion size linked to mood record

--- lib/main.dart
+++ lib/main.dart
@@ -1019,6 +1019,7 @@ class _HomeScreenState extends State<HomeScreen>
                         ],
                       ),
                     ),
+                   const SizedBox(height: 180), 
                 ],
               ),
             ),
